4125933da12c2461210ed935fcd25fac3c430685
MichaelSel <seltenmusic@gmail.com>
2021-12-08 01:03:25 +0200

    New Scripts

--- Scripts/1_Analysis/2_plotting.py
+++ Scripts/1_Analysis/2_plotting.py
@@ -1,15 +1,18 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib import pyplot
 
 from paths import *
 import pandas as pd
 
 
 data = pd.read_csv(processed_dir + 'totals.csv')
-data = data[data['chose']=="shifted"]
-# ax = sns.barplot(y="set", x=">subject>set>condition:bias", data=data)
-sns.scatterplot(y="set", x=">subject>set>condition:bias", data=data)
+data = data[data['chose']=="shifted"].reset_index()
 
+mean = data.groupby(['set'])[">subject>set>condition:bias"].mean().reset_index(name='bias')
+plot_order = mean.sort_values(by=["bias"]).set.values
+fig, ax = pyplot.subplots(figsize=(8,12))
+
+sns.pointplot(ax=ax, y="set", x=">subject>set>condition:bias", data=data, order=plot_order,height=15, aspect=11.7/2)
 
 plt.show()
-print('hello')
