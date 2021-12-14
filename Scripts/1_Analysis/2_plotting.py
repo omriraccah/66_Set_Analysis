da9b9ef56ac0045aee43e7b2e5030342cac2771f
MichaelSel <seltenmusic@gmail.com>
2021-12-14 03:58:54 +0200

    New Scripts

--- Scripts/1_Analysis/2_plotting.py
+++ Scripts/1_Analysis/2_plotting.py
@@ -5,6 +5,12 @@ from matplotlib import pyplot
 from paths import *
 import pandas as pd
 
+"""###
+
+WITHIN SUBJECT ANALYSIS
+
+###"""
+
 data = pd.read_csv(processed_dir + 'totals.csv')
 
 #subjects that selected "neither" more than half the time (on entire task regardless of set)
@@ -54,3 +60,31 @@ plot_order = data.groupby('set').mean().sort_values(by=["rate_NN_shifted"], asce
 fig, ax = pyplot.subplots(figsize=(8, 12))
 sns.pointplot(ax=ax, y="set", x="rate_NN_shifted", data=data, order=plot_order, height=15, aspect=11.7 / 2)
 plt.show()
+
+
+
+
+plot_order = data.groupby('set').mean().sort_values(by=["rate shifted"], ascending=False).index.values
+fig, ax = pyplot.subplots(figsize=(8, 12))
+sns.pointplot(ax=ax, y="set", x="rate shifted", data=data, order=plot_order, height=15, aspect=11.7 / 2)
+plt.show()
+
+"""###
+
+ACROSS SUBJECT ANALYSIS
+
+###"""
+across = pd.read_pickle(processed_dir + 'processed_data.pickle')
+across = across[across['has_decoy'] == False]
+
+counts = across.groupby('set')['chose'].value_counts().unstack()
+counts['total'] = counts['shifted'] + counts['swapped'] + counts['neither']
+counts['shifted_rate'] = counts['shifted'] / counts['total']
+counts['neither_rate'] = counts['neither'] / counts['total']
+counts['not_shifted_rate'] = 1 - counts['shifted_rate']
+counts = counts.reset_index().rename(columns={'index':'set'})
+plot_order = counts.sort_values(by=["neither_rate"], ascending=False)['set'].values
+fig, ax = pyplot.subplots(figsize=(8, 12))
+sns.pointplot(ax=ax, y="set", x="neither_rate", data=counts, order=plot_order, height=15, aspect=11.7 / 2)
+plt.show()
+
